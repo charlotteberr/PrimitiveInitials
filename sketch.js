28a89991b3f764068114be3a864eaa35d669b787
Charlotte Bertanzetti <cjbertanzetti@gmail.com>
2026-01-19 23:02:55 -0700

    Uploaded files

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(400, 400);
+  background(230)
+  noStroke();
+}
+
+function draw() {
+  //C
+  fill(0);
+  ellipse(170,200,150,200);
+  fill(230);
+  rect(170,100,150,300);
+  
+  //B
+  fill(0);
+  ellipse(230,150,130,100);
+  ellipse(230,250,130,100);
+  fill(230);
+  rect(170,100,70,300);
+  fill(0);
+  rect(220,100,10,200);
+  
+}
